d7dc5a64540b418a17c560f5b77c9fbda27bed1f
KaviKurtosys <95690055+KaviKurtosys@users.noreply.github.com>
2021-12-29 10:26:58 +0200

    Module 5
    Module 5

--- src/quiz.js
+++ src/quiz.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
+
 let quizData = require('./quiz_data.json')
 
-class Quiz extends Component{
-    constructor(props) {
-        super(props);
-
-        this.state = { quiz_position: 1 };
-    };
-
-    render() {
-        return <div>
-            <div className='QuizQuestion'>
-                { quizData.quiz_questions[0].instruction_text }
-            </div>
-        </div>;
-    }
+class Quiz extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { quiz_position: 1 }
+  }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 }
+    })
+  }
+  render() {
+    const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
+    return (
+      <div>
+        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+      </div>
+    )
+  }
 }
-
-// const test = new Quiz();
 
 export default Quiz
